3810b9bf5e7f2c686b69773ec5023bd8bce29493
codeTIT4N <lokeshkumar636@yahoo.com>
2022-11-19 04:00:38 +0530

    small cleanup

--- README.md
+++ README.md
@@ -6,9 +6,9 @@ The ERC20 contract is on the Polygon mumbai test network.
 
 Mint some ERC20 tokens for testing here: https://mint-nalnda.netlify.app/
 
-Now, rename the .env.example to .env and put your private key there.
+Now, rename the .env.example to .env and put your private key (without the 0x prefix) here.
 
 Run using:
 ```shell
-python example.py
+python script.py
 ```
--- script.py
+++ script.py
@@ -8,8 +8,8 @@ abi = json.load(abi_file)
 w3 = Web3(Web3.HTTPProvider('https://matic-mumbai.chainstacklabs.com'))
 
 pvt_key = os.getenv("PVT_KEY")
-signer = w3.eth.account.from_key('0x'+pvt_key)
-pub_key=signer.address
+signer = w3.eth.account.from_key('0x' + pvt_key)
+pub_key = signer.address
 print('Public key/Address',pub_key)
 
 #getting contract instance
@@ -17,22 +17,21 @@ contract_address='0xfEc014B41506430F055ceff9A007e690D409b304'
 erc20_contract =  w3.eth.contract(address=contract_address, abi=abi)
 # calling simple read functions
 balBef = erc20_contract.functions.balanceOf(pub_key).call()
-print('ERC20 Balance before:',balBef)
+print('ERC20 Balance before:',w3.fromWei(balBef,'ether'),'tokens')
 
 #inject middleware
 from web3.middleware import geth_poa_middleware
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 
-# MATIC mumbai chain
-chain_id = 80001
+chain_id = 80001 # Polygon Mumbai test chain
 nonce = w3.eth.getTransactionCount(pub_key)
-send_to='0xc9D506209f57948a0C0df6ED45621Fb47572Af99'
-transfer_amt=w3.toWei(1,'ether'); # 1 token
+send_to = '0xc9D506209f57948a0C0df6ED45621Fb47572Af99'
+transfer_amt = w3.toWei(1,'ether'); # 1 token
 
 try:
     # creating a transaction
-    transfer_txn = erc20_contract.functions.transfer(send_to,transfer_amt).buildTransaction({ "chainId":chain_id, "from":pub_key, "nonce":nonce})
+    transfer_txn = erc20_contract.functions.transfer(send_to,transfer_amt).buildTransaction({ "chainId": chain_id, "from": pub_key, "nonce": nonce})
     # signing transaction with pvt key
     signed_tx = w3.eth.account.sign_transaction(transfer_txn, pvt_key)
     # sending the transaction
@@ -46,4 +45,4 @@ except Exception as err:
 
 
 balAfter = erc20_contract.functions.balanceOf(pub_key).call()
-print('ERC20 Balance after:',balAfter)
+print('ERC20 Balance after:',w3.fromWei(balAfter,'ether'),'tokens')
